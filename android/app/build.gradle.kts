27b14c5e72e805d48aa0ae1171fb7106dc99a92d
CodeSnap Developer <codesnap@nightdev.io>
2026-06-14 01:41:37 +0000

    chore: bump version to 1.4.7+19

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "ru.maxspeed.maxspeed_vpn"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.4.6+18"
+        versionCode = 19
+        versionName = "1.4.7+19"
         ndk {
             abiFilters.add("arm64-v8a")
         }
